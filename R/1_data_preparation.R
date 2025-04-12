bff6962e08ee07006f56e5ceca17cc64c579b3fc
DAChristina <d.christina23@imperial.ac.uk>
2025-04-12 22:17:44 +0700

    generate virulence factors table

--- R/1_data_preparation.R
+++ R/1_data_preparation.R
@@ -180,37 +180,76 @@ cdc_result_wide
 
 
 # virulence profile
-vir_nt_header <- read.table("inputs/prepare_vfdb_database/VFDB_setA_nt_compiled_headers_nt.txt",
-                            header = F, sep = "\t") %>% 
-  dplyr::rename(header = V1) %>% 
-  dplyr::mutate(gene    = str_extract(header, "(?<= \\()[^\\)]+(?=\\) )"), # " (" & ") "
-                gene_id = str_extract(header, "^[^)]*\\)"), # begin & ")"
-                species = str_extract(header, "(?<=\\] \\[)[^\\]]+(?=\\])") # "] [" and "]"
-                ) %>% 
-  # view() %>% 
-  glimpse()
-
-vir_pro_header <- read.table("inputs/prepare_vfdb_database/VFDB_setA_nt_compiled_headers_pro.txt",
-                            header = F, sep = "\t") %>% 
-  dplyr::rename(header = V1) %>% 
-  dplyr::mutate(gene    = str_extract(header, "(?<= \\()[^\\)]+(?=\\) )"), # " (" & ") "
-                gene_id = str_extract(header, "^[^)]*\\)"), # begin & ")"
-                species = str_extract(header, "(?<=\\] \\[)[^\\]]+(?=\\])") # "] [" & "]"
+vir_combined_results <- dplyr::left_join(
+  read.table("outputs/result_blast_virulences/blastn_tabular_setA_nt_VFDB.txt",
+             header = F, sep = "\t") %>% 
+    stats::setNames(c("file_name", "qseqid", "temporary_sseqid",
+                      "nt_pident", "nt_length", "nt_mismatch",
+                      "nt_gapopen", "nt_qstart", "nt_qend",
+                      "nt_sstart", "nt_send",
+                      "nt_evalue", "nt_bitscore")) %>% 
+    dplyr::left_join(
+      read.table("inputs/prepare_vfdb_database/VFDB_setA_compiled_headers_nt.txt",
+                 header = F, sep = "\t") %>% 
+        dplyr::rename(header = V1) %>% 
+        dplyr::mutate(gene    = str_extract(header, "(?<= \\()[^\\)]+(?=\\) )"), # " (" & ") "
+                      gene_id = str_extract(header, "^[^)]*\\)"), # begin & ")"
+                      species = str_extract(header, "(?<=\\] \\[)[^\\]]+(?=\\])") # "] [" and "]"
+        )
+      ,
+      by = c("temporary_sseqid" = "gene_id")
+    ) %>% 
+    dplyr::filter(file_name == "Streptococcus_pneumoniae_RMD131_contigs_from_YM")
+  ,
+  read.table("outputs/result_blast_virulences/blastx_tabular_setA_aa_VFDB.txt",
+             header = F, sep = "\t") %>% 
+    stats::setNames(c("file_name", "qseqid", "temporary_sseqid",
+                      "aa_pident", "aa_length", "aa_mismatch",
+                      "aa_gapopen", "aa_qstart", "aa_qend",
+                      "aa_sstart", "aa_send",
+                      "aa_evalue", "aa_bitscore")) %>% 
+    dplyr::left_join(
+      read.table("inputs/prepare_vfdb_database/VFDB_setA_compiled_headers_pro.txt",
+                 header = F, sep = "\t") %>% 
+        dplyr::rename(header = V1) %>% 
+        dplyr::mutate(gene    = str_extract(header, "(?<= \\()[^\\)]+(?=\\) )"), # " (" & ") "
+                      gene_id = str_extract(header, "^[^)]*\\)"), # begin & ")"
+                      species = str_extract(header, "(?<=\\] \\[)[^\\]]+(?=\\])") # "] [" & "]"
+        )
+      ,
+      by = c("temporary_sseqid" = "gene_id")
+    ) %>% 
+    dplyr::filter(file_name == "Streptococcus_pneumoniae_RMD131_contigs_from_YM")
+  ,
+  by = c("file_name", "qseqid", "temporary_sseqid")
+) %>% 
+  dplyr::arrange(dplyr::desc(aa_pident)) %>% 
+  dplyr::distinct(file_name, temporary_sseqid, .keep_all = T) %>% 
+  dplyr::select(-contains(".y")) %>% 
+  dplyr::mutate(
+    gene_present = case_when(
+      nt_pident >= 80 ~ "present",
+      TRUE ~ "absent"
+    ),
+    protein_function = case_when(
+      aa_pident >= 95 & aa_gapopen == 0 ~ "functional",
+      aa_pident >= 90 & aa_gapopen <= 5 & aa_mismatch <= 30 ~ "variant",
+      aa_pident <= 85 & aa_mismatch >= 25 ~ "possibly defective",
+      TRUE ~ "possibly defective"
+    ),
+    bacwgs_check = case_when(
+      gene.x %in% c("cbpD", "cps4A", "cps4B", "cps4D", "hysA", "lytA", "lytC", 
+                    "nanA", "nanB", "pavA", "pce/cbpE", "pfbA", "ply", "psaA") ~ "detected",
+      TRUE ~ "no"
+    )
   ) %>% 
   # view() %>% 
   glimpse()
 
-vir_nt_result <- read.table("outputs/result_blast_virulences/blastn_tabular_setA_high_confidence_VFDB.txt",
-                            header = F, sep = "\t") %>% 
-  stats::setNames(c("qseqid", "temporary_sseqid", "pident", "length", "mismatch",
-             "gapopen", "qstart", "qend", "sstart", "send",
-             "evalue", "bitscore")) %>% 
-  dplyr::left_join(
-    vir_nt_header,
-    by = c("temporary_sseqid" = "gene_id")
-  ) %>% 
-  # dplyr::mutate() %>% 
-  glimpse()
+test <- vir_combined_results %>% 
+  select(nt_pident, aa_pident, aa_mismatch, aa_gapopen, gene.x, species.x,
+         gene_present, protein_function, bacwgs_check) %>% 
+  view()
 
 
 # competence genes profile
