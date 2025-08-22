ecc3794f70ec75a07b280ed622b9e90b14b459ef
DAChristina <d.christina23@imperial.ac.uk>
2025-08-22 08:38:11 +0700

    clean up code

--- R/2_data_viz.R
+++ R/2_data_viz.R
@@ -36,88 +36,9 @@ coms <- data.frame(
   # view() %>% 
   glimpse()
 
-
-
 gff <- read.delim("/home/ron/pneumo_2025_serotype1_brief_report/outputs/result_prokka_combined_pathogenWatch_monocle/Streptococcus_pneumoniae_RMD131_contigs_from_YM.gff",
                   comment.char = "#", header = FALSE) %>% 
   stats::setNames(c("seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes")) %>% 
   dplyr::filter(grepl("comA|comB|comC|comD|comE|comX|comEA|comEC|comGC|coiA|=com|gene=com", attributes)) %>% 
   # view() %>% 
   glimpse()
-
-
-
-
-library(circlize)
-
-# Example genome size
-genome_size <- 2000000
-
-phages <- readLines("/home/ron/pneumo_2025_serotype1_brief_report/outputs/result_phastest/phastest_contigs/ZZ_9db106ee3a.PHASTEST/phage_regions.fna") %>% 
-  grep("^>", ., value = TRUE) %>% 
-  glimpse()
-
-phage_df <- do.call(rbind, lapply(phages, function(header) {
-  region <- sub("^>\\S+\\s+([^:]+):.*", "\\1", header)
-  
-  loc_match <- regmatches(header, regexec(":(\\d+)-(\\d+)", header))[[1]]
-  start <- as.integer(loc_match[2])
-  end <- as.integer(loc_match[3])
-  
-  data.frame(region = region, start = start, end = end, stringsAsFactors = FALSE)
-})) %>% 
-  glimpse()
-
-circos.clear()
-circos.initialize(factors = "chr", xlim = c(0, genome_size))
-
-# Baseline genome track
-circos.track(ylim = c(0, 1), panel.fun = function(x, y) {
-  circos.axis(labels.cex = 0.01)  # Optional: axis labels
-}, track.height = 0.05, bg.border = NA)
-
-
-circos.trackPlotRegion(ylim = c(0, 1), track.height = 0.05, bg.border = NA,
-                       panel.fun = function(x, y) {
-                         circos.rect(phage_df$start, 0, phage_df$end, 1, col = "purple", border = "purple")
-                       })
-
-circos.trackPlotRegion(ylim = c(0, 1), track.height = 0.05, bg.border = NA,
-                       panel.fun = function(x, y) {
-                         circos.rect(coms$start, 0, coms$end, 1, col = "blue", border = "blue")
-                       })
-
-# Clear after plot
-circos.clear()
-
-
-
-
-
-
-regions <- dplyr::bind_rows(
-  phage_df %>% 
-    dplyr::mutate(category = "Phage")
-  ,
-  coms %>% 
-    dplyr::mutate(category = "Competence")
-)
-
-regions <- regions %>%
-  mutate(y = as.numeric(factor(category, levels = unique(category))))
-
-# Plot
-ggplot(regions) +
-  geom_segment(aes(x = start, xend = end, y = y, yend = y, color = category), size = 6) +
-  geom_text(aes(x = (start + end)/2, y = y + 0.3, label = gene), size = 3, vjust = 0) +
-  scale_y_continuous(breaks = unique(regions$y), labels = unique(regions$category)) +
-  scale_x_continuous("Genome Position (bp)", expand = expansion(mult = c(0.01, 0.01))) +
-  labs(y = "Feature Type", title = "Linear Genome Annotation") +
-  theme_bw() +
-  theme(
-    panel.grid.minor = element_blank(),
-    axis.title.y = element_blank(),
-    axis.text.y = element_text(size = 10),
-    legend.position = "none"
-  )
-
